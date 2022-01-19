9d46bac11c6f7bf4fe222f96b2d36aa1d0a6d80d
Chansik Yun <chansigi@gmail.com>
2022-01-19 14:07:29 +0900

    .

--- 16_포인터와함수2.c
+++ 16_포인터와함수2.c
@@ -6,13 +6,9 @@
 
 void swap(int* a, int* b)
 {
-    // int temp = *a;
-    // *a = *b;
-    // *b = temp;
-
-    int* temp = a;
-    a = b;
-    b = temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 int main()
